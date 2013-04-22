b5ae608b2291960fd282457ffe2eae35deb0940c
illnino ho <illnino.giggs@gmail.com>
2013-04-22 22:16:52 +0800

    initial setup, downgrade jquery-rails 2.1.4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mzvisa::Application.config.secret_token = 'ab43afcf06d2bce02057d9d4839416d0b862b9373657042e33ae12be3cf12d2625cba1ed9b2ad585b89690e1afcca8d242408ee68b53c9e75a711bede22a8afa'
