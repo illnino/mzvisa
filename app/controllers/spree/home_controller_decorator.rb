3b3380f6ab7df59ba6dffc1f743c349839bf4341
illnino ho <illnino.giggs@gmail.com>
2013-04-28 15:40:24 +0800

    promotions news

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -3,6 +3,9 @@ Spree::HomeController.class_eval do
   def index
     slider = Spree::Taxon.where(:name => 'Slider').first
     @slider_products = slider.products.active if slider
+
+    promotion= Spree::Taxon.where(:name => 'Promotion').first
+    @promotion_products = promotion.products.active if promotion
   end
 
 end
