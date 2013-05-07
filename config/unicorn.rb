095aef9eda21c6058a38ed039ffb4a0b67625389
illnino ho <illnino.giggs@gmail.com>
2013-05-08 00:06:14 +0800

    add shared path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,7 +16,7 @@ preload_app true
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
  
-pid "/tmp/unicorn.mzvisa.pid"
+pid "/home/spree/mzvisa/shared/tmp/unicorn.mzvisa.pid"
  
 # Production specific settings
 if env == "production"
