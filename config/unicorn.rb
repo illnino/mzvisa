8bb1f9a320bee2ddd460bca693766f5aac8ba888
illnino ho <illnino.giggs@gmail.com>
2013-05-08 00:20:01 +0800

    shared/pids

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,7 +16,7 @@ preload_app true
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
  
-pid "/home/spree/mzvisa/shared/tmp/unicorn.mzvisa.pid"
+pid "/home/spree/mzvisa/shared/tmp/pids/unicorn.mzvisa.pid"
  
 # Production specific settings
 if env == "production"
