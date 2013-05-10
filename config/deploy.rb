5bc3fbc23b732eb7b400ead523fa51f5600c4eff
illnino ho <illnino.giggs@gmail.com>
2013-05-10 18:36:57 +0800

    tiny fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,7 @@ namespace :deploy do
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
+    # run "rm #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
