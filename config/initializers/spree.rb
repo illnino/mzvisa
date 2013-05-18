5b606dbff908aa571dab7599d4d998d415e633e6
illnino ho <illnino.giggs@gmail.com>
2013-05-18 20:50:43 +0800

    redesign layout

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,6 +10,7 @@ Spree.config do |config|
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
   config.allow_ssl_in_production = false
+  # config.products_per_page = 100
 end
 
 Spree.user_class = "Spree::User"
