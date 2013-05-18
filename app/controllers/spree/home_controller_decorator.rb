6ef570ee8dfbb7009301c16691a6515f9c76b1cf
illnino ho <illnino.giggs@gmail.com>
2013-05-18 22:49:44 +0800

    flexslider offset

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,13 +1,14 @@
 Spree::HomeController.class_eval do
 
   def index
-    slider = Spree::Taxon.where(:name => 'Slider').first
-    @slider_products = slider.products.active if slider
+    # slider = Spree::Taxon.where(:name => 'Slider').first
+    # @slider_products = slider.products.active if slider
 
     promotion= Spree::Taxon.where(:name => 'Promotion').first
     @promotion_products = promotion.products.active if promotion
 
-    @news = Spree::Post.published
+    # @news = Spree::Post.published
+    @uploads = Spree::Upload.where(:alt => 'home')
   end
 
 end
